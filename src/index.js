f7d0cc5c8dfc2915a92f531778bcae5228e49059
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-02-25 16:22:00 +0100

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    const comm = /,/gi;
+    const minus = /-/gi;
+    const k = n.toString();
+    
+    if(n >= 0) {
+    const newString = k.split("").reverse().join().replace(comm, '');
+    return(newString);
+    }
+    if (n < 0) {
+        const m = k.replace(minus, '');
+        const newString = m.split("").reverse().join().replace(comm, '');
+        return(newString);
+    }
+    
 }
